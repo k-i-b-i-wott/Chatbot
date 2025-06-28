1f7c6f2e9edfba18528e48d3f1e14559c985a4a7
Kibiwott <bk.kibiwott@gmail.com>
2025-06-28 13:20:49 +0300

    Completed the RAG medical chatbot

--- store_index.py
+++ store_index.py
@@ -8,7 +8,7 @@ import os
 load_dotenv()
 
 PINECONE_API_KEY =os.environ.get('PINECONE_API_KEY')
-os.environ.get('PINECONE_API_KEY') = PINECONE_API_KEY
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 
 
 extracted_data = load_pdf_file(data='Data/')
@@ -35,3 +35,4 @@ docsearch = PineconeVectorStore.from_documents(
     index_name= index_name,
     embedding= embeddings
 )
+
